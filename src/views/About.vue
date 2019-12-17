fc9d3758be619740e67513f1ec68a3aefd163a12
Megastary <megastary@houby-studio.eu>
2019-12-17 22:22:26 +0100

    Add style to about page and bump version
    Last update broke updates, should fix this

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,52 @@
 <template>
   <div class="profile">
-    <h1>About</h1>
-    <p>Made with <v-icon>mdi-heart</v-icon></p>
-    <p>OGL Version {{ version }}</p>
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-card>
+            <v-card-title>
+              <h1>About</h1>
+            </v-card-title>
+            <v-card-subtitle>
+              Made with <v-icon>mdi-heart</v-icon>
+            </v-card-subtitle>
+            <v-card-text>
+              <v-row>
+                <v-col cols="12" md="6">
+                  <p>OGL Version {{ version }}</p>
+                  <span>Scheme</span>
+                  <v-switch v-model="$vuetify.theme.dark" primary label="Dark" />
+                </v-col>
+                <v-col cols="12" md="6">
+                  <span>Drawer</span>
+                  <v-radio-group v-model="primaryDrawer.type" column>
+                    <v-radio
+                      v-for="drawer in drawers"
+                      :key="drawer"
+                      :label="drawer"
+                      :value="drawer.toLowerCase()"
+                      primary
+                    />
+                  </v-radio-group>
+                  <v-switch v-model="primaryDrawer.clipped" label="Clipped" primary />
+                  <v-switch v-model="primaryDrawer.floating" label="Floating" primary />
+                  <v-switch v-model="primaryDrawer.mini" label="Mini" primary />
+                </v-col>
+                <v-col cols="12" md="6">
+                  <span>Footer</span>
+                  <v-switch label="Inset" primary />
+                </v-col>
+              </v-row>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn text>Cancel</v-btn>
+              <v-btn text color="primary">Submit</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -15,6 +59,14 @@ export default {
     // Header
   },
   data: () => ({
+    drawers: ['Default (no property)', 'Permanent', 'Temporary'],
+    primaryDrawer: {
+      model: null,
+      type: 'default (no property)',
+      clipped: false,
+      floating: false,
+      mini: false
+    },
     version: remote.app.getVersion()
   })
 }
