adab7a376173f1faeda91ed250aa94cca9997889
Megastary <megastary@houby-studio.eu>
2019-12-17 05:32:12 +0100

    First Vue app test

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="header">
+        <h1>Game Library index</h1>
+        <p>{{ msg }} a</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  color: blueviolet;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Library.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="library">
+    <Header msg="Yeah baby"/>
+    <p>Here resides view</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'library',
+  components: {
+    Header
+  }
+}
+</script>
